2f01f70b2c781bfb5287f3702d2c4b3f4da26e6d
anushka1822 <anushkatyagi97580@gmail.com>
2025-12-01 13:28:31 +0530

    added backend url

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_URL = import.meta.env.VITE_API_URL || 'http://127.0.0.1:8000/api/v1';
+const API_URL = import.meta.env.VITE_API_URL || 'https://safahome-assignment-anushka.onrender.com/api/v1';
 
 const api = axios.create({
     baseURL: API_URL,
